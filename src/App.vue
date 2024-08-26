2535d6843344299900e01688d663b626dcbebc5b
Sandra <a0960529965@gmail.com>
2024-08-26 14:33:13 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="container">
+    <HelloWorld msg="Vite + Vue" />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 100%;
+}
+</style>
